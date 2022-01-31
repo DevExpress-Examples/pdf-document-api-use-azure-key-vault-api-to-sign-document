ea7d3d6647b44982761f1d9c68bc7ea5525593cf
DevExpressExampleBot <fakeEmail>
2022-01-31 18:04:54 +0300

    Source auto update [skip ci]

--- VB/PdfAPIAzureKeyVaultSample/AzureKeyVaultClient.vb
+++ VB/PdfAPIAzureKeyVaultSample/AzureKeyVaultClient.vb
@@ -1,3 +1,4 @@
+#Region "#using"
 Imports Azure.Identity
 Imports Azure.Security.KeyVault.Certificates
 Imports Azure.Security.KeyVault.Keys
@@ -9,54 +10,10 @@ Imports System
 Imports System.Collections.Generic
 Imports System.Diagnostics
 
+#End Region  ' #using
 Namespace PdfAPIAzureKeyVaultSample
 
-    Public Class AzureKeyVaultSigner
-        Inherits Pkcs7SignerBase
-
-        'OID for RSA signing algorithm:
-        Const PKCS1RsaEncryption As String = "1.2.840.113549.1.1.1"
-
-        Private ReadOnly keyVaultClient As AzureKeyVaultClient
-
-        Private ReadOnly keyId As String
-
-        Private ReadOnly certificateChain As Byte()()
-
-        'Must match with key algorithm (RSA or ECDSA)
-        'For RSA PKCS1RsaEncryption(1.2.840.113549.1.1.1) OID can be used with any digest algorithm
-        'For ECDSA use OIDs from this family http://oid-info.com/get/1.2.840.10045.4.3 
-        'Specified digest algorithm must be same with DigestCalculator algorithm.
-        Protected Overrides ReadOnly Property DigestCalculator As IDigestCalculator
-            Get
-                Return New DigestCalculator(HashAlgorithmType.SHA256)
-            End Get
-        End Property 'Digest algorithm
-
-        Protected Overrides ReadOnly Property SigningAlgorithmOID As String
-            Get
-                Return PKCS1RsaEncryption
-            End Get
-        End Property
-
-        Protected Overrides Function GetCertificates() As IEnumerable(Of Byte())
-            Return certificateChain
-        End Function
-
-        Public Sub New(ByVal keyVaultClient As AzureKeyVaultClient, ByVal certificateIdentifier As String, ByVal keyId As String, ByVal keyVaultUri As String, ByVal Optional tsaClient As ITsaClient = Nothing, ByVal Optional ocspClient As IOcspClient = Nothing, ByVal Optional crlClient As ICrlClient = Nothing, ByVal Optional profile As PdfSignatureProfile = PdfSignatureProfile.Pdf)
-            MyBase.New(tsaClient, ocspClient, crlClient, profile)
-            Me.keyVaultClient = keyVaultClient
-            Me.keyId = keyId
-            'Get certificate (without piblic key) from Azure Key Vault storage via CertificateClient API or create a new one at runtime
-            'You can get the whole certificate chain here
-            certificateChain = New Byte()() {keyVaultClient.GetCertificateData(keyVaultUri, certificateIdentifier)}
-        End Sub
-
-        Protected Overrides Function SignDigest(ByVal digest As Byte()) As Byte()
-            Return keyVaultClient.Sign(keyId, SignatureAlgorithm.RS256, digest)
-        End Function
-    End Class
-
+#Region "#client"
     Public Class AzureKeyVaultClient
 
         Public Shared Function CreateClient(ByVal keyVaultUrl As String) As AzureKeyVaultClient
@@ -87,4 +44,54 @@ Namespace PdfAPIAzureKeyVaultSample
             Return cert.Cer
         End Function
     End Class
+
+#End Region  ' #client
+#Region "#signer"
+    Public Class AzureKeyVaultSigner
+        Inherits Pkcs7SignerBase
+
+        ' OID for RSA signing algorithm:
+        Const PKCS1RsaEncryption As String = "1.2.840.113549.1.1.1"
+
+        Private ReadOnly keyVaultClient As AzureKeyVaultClient
+
+        Private ReadOnly keyId As String
+
+        Private ReadOnly certificateChain As Byte()()
+
+        ' Must match with key algorithm (RSA or ECDSA)
+        ' For RSA PKCS1RsaEncryption(1.2.840.113549.1.1.1) OID can be used with any digest algorithm
+        ' For ECDSA use OIDs from this family http://oid-info.com/get/1.2.840.10045.4.3 
+        ' Specified digest algorithm must be same with DigestCalculator algorithm.
+        Protected Overrides ReadOnly Property DigestCalculator As IDigestCalculator
+            Get
+                Return New DigestCalculator(HashAlgorithmType.SHA256)
+            End Get
+        End Property 'Digest algorithm
+
+        Protected Overrides ReadOnly Property SigningAlgorithmOID As String
+            Get
+                Return PKCS1RsaEncryption
+            End Get
+        End Property
+
+        Protected Overrides Function GetCertificates() As IEnumerable(Of Byte())
+            Return certificateChain
+        End Function
+
+        Public Sub New(ByVal keyVaultClient As AzureKeyVaultClient, ByVal certificateIdentifier As String, ByVal keyId As String, ByVal keyVaultUri As String, ByVal Optional tsaClient As ITsaClient = Nothing, ByVal Optional ocspClient As IOcspClient = Nothing, ByVal Optional crlClient As ICrlClient = Nothing, ByVal Optional profile As PdfSignatureProfile = PdfSignatureProfile.Pdf)
+            MyBase.New(tsaClient, ocspClient, crlClient, profile)
+            Me.keyVaultClient = keyVaultClient
+            Me.keyId = keyId
+            ' Get certificate (without a public key) from Azure Key Vault storage
+            ' via CertificateClient API or create a new one at runtime
+            ' You can get the whole certificate chain here
+            certificateChain = New Byte()() {keyVaultClient.GetCertificateData(keyVaultUri, certificateIdentifier)}
+        End Sub
+
+        Protected Overrides Function SignDigest(ByVal digest As Byte()) As Byte()
+            Return keyVaultClient.Sign(keyId, SignatureAlgorithm.RS256, digest)
+        End Function
+    End Class
+#End Region
 End Namespace
