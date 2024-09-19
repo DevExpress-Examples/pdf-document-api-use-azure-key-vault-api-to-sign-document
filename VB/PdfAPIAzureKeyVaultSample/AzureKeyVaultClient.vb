1966583fa6c1d8e9ac063ab8d4922ba70e4f04dd
guemjalee <polina.fedorova@devexpress.com>
2024-09-19 16:20:20 +0200

    update vb

--- VB/PdfAPIAzureKeyVaultSample/AzureKeyVaultClient.vb
+++ VB/PdfAPIAzureKeyVaultSample/AzureKeyVaultClient.vb
@@ -1,4 +1,7 @@
+﻿Option Infer On
+
 #Region "#using"
+Imports Azure.Core
 Imports Azure.Identity
 Imports Azure.Security.KeyVault.Certificates
 Imports Azure.Security.KeyVault.Keys
@@ -9,89 +12,121 @@ Imports DevExpress.Pdf
 Imports System
 Imports System.Collections.Generic
 Imports System.Diagnostics
-
+Imports System.Security.Cryptography.X509Certificates
 #End Region
+
+
+
 Namespace PdfAPIAzureKeyVaultSample
+	#Region "client"
+	Public Class AzureKeyVaultClient
+		Public Shared Function CreateClient(ByVal keyVaultUri As Uri) As AzureKeyVaultClient
+			Dim tokenCredential = New DefaultAzureCredential(New DefaultAzureCredentialOptions With {
+				.ExcludeInteractiveBrowserCredential = False,
+				.ExcludeVisualStudioCodeCredential = True
+			})
 
-#Region "client"
-    Public Class AzureKeyVaultClient
+			Return New AzureKeyVaultClient(New KeyClient(keyVaultUri, tokenCredential), tokenCredential)
+		End Function
 
-        Public Shared Function CreateClient(ByVal keyVaultUrl As String) As AzureKeyVaultClient
-            Return New AzureKeyVaultClient(New KeyClient(New Uri(keyVaultUrl), New DefaultAzureCredential()))
-        End Function
+		Private ReadOnly client As KeyClient
 
-        Private ReadOnly client As KeyClient
+		Private clientCredentials As TokenCredential
+		Private Sub New(ByVal client As KeyClient, ByVal cryptoClientCredential As TokenCredential)
+			Me.client = client
+			Me.clientCredentials = cryptoClientCredential
+		End Sub
+		Public Function Sign(ByVal certificateName As String, ByVal algorithm As SignatureAlgorithm, ByVal digest() As Byte, Optional ByVal version As String = Nothing) As Byte()
+			Dim cloudRsaKey As KeyVaultKey = client.GetKey(certificateName, version:= version)
+			Dim rsaCryptoClient = New CryptographyClient(cloudRsaKey.Id, clientCredentials)
 
-        Private defaultAzureCredential As DefaultAzureCredential
+			Dim rsaSignResult As SignResult = rsaCryptoClient.Sign(algorithm, digest)
+			Debug.WriteLine($"Signed digest using the algorithm {rsaSignResult.Algorithm}, " & $"with key {1}. The resulting signature is {2}")
+			Return rsaSignResult.Signature
+		End Function
 
-        Private Sub New(ByVal client As KeyClient)
-            Me.client = client
-            Dim credentialOptions = New DefaultAzureCredentialOptions With {.ExcludeInteractiveBrowserCredential = False, .ExcludeVisualStudioCodeCredential = True}
-            defaultAzureCredential = New DefaultAzureCredential(credentialOptions)
-        End Sub
+		Public Function GetCertificateData(ByVal keyId As String) As KeyVaultCertificateWithPolicy
+			Dim certificateClient = New CertificateClient(client.VaultUri, clientCredentials)
+			Dim cert As KeyVaultCertificateWithPolicy = certificateClient.GetCertificate(keyId)
+			Return cert
+		End Function
+	End Class
+	#End Region
 
-        Public Function Sign(ByVal keyId As String, ByVal algorithm As SignatureAlgorithm, ByVal digest As Byte()) As Byte()
-            Dim cloudRsaKey As KeyVaultKey = client.GetKey(keyId)
-            Dim rsaCryptoClient = New CryptographyClient(cloudRsaKey.Id, defaultAzureCredential)
-            Dim rsaSignResult As SignResult = rsaCryptoClient.Sign(algorithm, digest)
-            Call Debug.WriteLine($"Signed digest using the algorithm {rsaSignResult.Algorithm}, with key {rsaSignResult.KeyId}. " & $"The resulting signature is {Convert.ToBase64String(rsaSignResult.Signature)}")
-            Return rsaSignResult.Signature
-        End Function
+	#Region "signer"
+	Public Class AzureKeyVaultSigner
+		Inherits Pkcs7SignerBase
 
-        Public Function GetCertificateData(ByVal keyVaultUrl As String, ByVal certificateIdentifier As String) As Byte()
-            Dim certificateClient = New CertificateClient(New Uri(keyVaultUrl), defaultAzureCredential)
-            Dim cert As KeyVaultCertificateWithPolicy = certificateClient.GetCertificate(certificateIdentifier)
-            Return cert.Cer
-        End Function
-    End Class
+		'OID for RSA signing algorithm:
+		Private Const PKCS1RsaEncryption As String = "1.2.840.113549.1.1.1"
 
-#End Region
-#Region "signer"
-    Public Class AzureKeyVaultSigner
-        Inherits Pkcs7SignerBase
+		Private ReadOnly keyVaultClient As AzureKeyVaultClient
+		Private ReadOnly certificate As KeyVaultCertificateWithPolicy
 
-        ' OID for RSA signature algorithm:
-        Const PKCS1RsaEncryption As String = "1.2.840.113549.1.1.1"
+		'Must match with key algorithm (RSA or ECDSA)
+		'For RSA PKCS1RsaEncryption(1.2.840.113549.1.1.1) OID can be used with any digest algorithm
+		'For ECDSA use OIDs from this family http://oid-info.com/get/1.2.840.10045.4.3 
+		'Specified digest algorithm must be same with DigestCalculator algorithm.
+		Protected Overrides ReadOnly Property DigestCalculator() As IDigestCalculator 'Digest algorithm
+			Get
+				Return New DigestCalculator(HashAlgorithmType.SHA256)
+			End Get
+		End Property
+		Protected Overrides ReadOnly Property SigningAlgorithmOID() As String
+			Get
+				Return PKCS1RsaEncryption
+			End Get
+		End Property
 
-        Private ReadOnly keyVaultClient As AzureKeyVaultClient
+		''' <summary>
+		''' Construct an instance of AzureKeyVaultSigner
+		''' </summary>
+		''' <param name="keyVaultClient">API client used to communicate with </param>
+		''' <param name="certificateName">The name of the Azure Certificate, will not contain any slashes</param>
+		''' <param name="certificateVersion">The version of the Azure Certificate, looks like a UUID. Leave empty/null to use the version labelled in Azure Portal as the "Current Version"</param>
+		''' <param name="tsaClient"></param>
+		''' <param name="ocspClient"></param>
+		''' <param name="crlClient"></param>
+		''' <param name="profile"></param>
+		''' <exception cref="System.ArgumentException"></exception>
+		Public Sub New(ByVal keyVaultClient As AzureKeyVaultClient, ByVal certificateName As String, Optional ByVal certificateVersion As String = Nothing, Optional ByVal tsaClient As ITsaClient = Nothing, Optional ByVal ocspClient As IOcspClient = Nothing, Optional ByVal crlClient As ICrlClient = Nothing, Optional ByVal profile As PdfSignatureProfile = PdfSignatureProfile.PAdES_BES)
+			MyBase.New(tsaClient, ocspClient, crlClient, profile)
+			If String.IsNullOrEmpty(certificateName) Then
+				Throw New System.ArgumentException("Certificate name must not be null or empty.")
+			End If
 
-        Private ReadOnly keyId As String
+			If certificateName.Contains("/"c) Then
+				Throw New System.ArgumentException("Invalid certificate name. Certificate name must not contain '/' character.")
+			End If
 
-        Private ReadOnly certificateChain As Byte()()
+			If certificateVersion IsNot Nothing AndAlso certificateVersion.Contains("/"c) Then
+				Throw New System.ArgumentException("Invalid certificate version. Certificate version must not contain '/' character.")
+			End If
 
-        ' Must match with key algorithm (RSA or ECDSA)
-        ' OID for RSA PKCS1RsaEncryption(1.2.840.113549.1.1.1) can have any digest algorithm
-        ' For ECDSA, use OIDs from this family: http://oid-info.com/get/1.2.840.10045.4.3 
-        ' Specified digest algorithm must be the same as to DigestCalculator algorithm
-        Protected Overrides ReadOnly Property DigestCalculator As IDigestCalculator
-            Get
-                Return New DigestCalculator(HashAlgorithmType.SHA256)
-            End Get
-        End Property 'Digest algorithm
+			Me.keyVaultClient = keyVaultClient
+			'Get certificate (without public key) via GetCertificateAsync API
+			'You can get the whole certificate chain here
+			Me.certificate = keyVaultClient.GetCertificateData($"{certificateName}/{certificateVersion}")
+		End Sub
 
-        Protected Overrides ReadOnly Property SigningAlgorithmOID As String
-            Get
-                Return PKCS1RsaEncryption
-            End Get
-        End Property
+		Protected Overrides Function GetCertificates() As IEnumerable(Of Byte())
+			Dim certificateChain As New List(Of Byte())()
+			Dim x509 = New X509Certificate2(certificate.Cer)
+			Dim chain = New X509Chain()
+			If True Then
+				chain.Build(x509)
 
-        Protected Overrides Function GetCertificates() As IEnumerable(Of Byte())
-            Return certificateChain
-        End Function
+				For Each item In chain.ChainElements
+					certificateChain.Add(item.Certificate.GetRawCertData())
+				Next item
+			End If
+			Return certificateChain
+		End Function
 
-        Public Sub New(ByVal keyVaultClient As AzureKeyVaultClient, ByVal certificateIdentifier As String, ByVal keyId As String, ByVal keyVaultUri As String, ByVal Optional tsaClient As ITsaClient = Nothing, ByVal Optional ocspClient As IOcspClient = Nothing, ByVal Optional crlClient As ICrlClient = Nothing, ByVal Optional profile As PdfSignatureProfile = PdfSignatureProfile.Pdf)
-            MyBase.New(tsaClient, ocspClient, crlClient, profile)
-            Me.keyVaultClient = keyVaultClient
-            Me.keyId = keyId
-            ' Get certificate (without a public key) from Azure Key Vault storage
-            ' or create a new one at runtime
-            ' You can get the whole certificate chain here
-            certificateChain = New Byte()() {keyVaultClient.GetCertificateData(keyVaultUri, certificateIdentifier)}
-        End Sub
-
-        Protected Overrides Function SignDigest(ByVal digest As Byte()) As Byte()
-            Return keyVaultClient.Sign(keyId, SignatureAlgorithm.RS256, digest)
-        End Function
-    End Class
-#End Region
+		Protected Overrides Function SignDigest(ByVal digest() As Byte) As Byte()
+			Dim signature = keyVaultClient.Sign(certificate.Name, SignatureAlgorithm.RS256, digest)
+			Return signature
+		End Function
+	End Class
+	#End Region
 End Namespace
--- VB/PdfAPIAzureKeyVaultSample/Program.vb
+++ VB/PdfAPIAzureKeyVaultSample/Program.vb
@@ -1,45 +1,58 @@
+﻿Option Infer On
+
 #Region "using"
 Imports DevExpress.Office.DigitalSignatures
 Imports DevExpress.Office.Tsp
 Imports DevExpress.Pdf
 Imports System
 Imports System.Diagnostics
-
 #End Region
+
 Namespace PdfAPIAzureKeyVaultSample
+	Friend Class Program
+		Shared Sub Main(ByVal args() As String)
+'			#Region "main"
+			Using signer = New PdfDocumentSigner("Document.pdf")
+				' Create a timestamp:
+				Dim tsaClient As ITsaClient = New TsaClient(New Uri("https://freetsa.org/tsr"), HashAlgorithmType.SHA256)
 
-    Friend Class Program
+				' Specify the signature's field name and location:
+				Dim pageNumber As Integer = 1
+				Dim description = New PdfSignatureFieldInfo(pageNumber)
+				description.Name = "SignatureField"
+				description.SignatureBounds = New PdfRectangle(10, 10, 50, 150)
 
-        Shared Sub Main(ByVal args As String())
-#Region "main"
-            Using signer = New PdfDocumentSigner("Document.pdf")
-                ' Create a timestamp:
-                Dim tsaClient As ITsaClient = New TsaClient(New Uri("https://freetsa.org/tsr"), HashAlgorithmType.SHA256)
-                ' Specify the signature's field name and location:
-                Dim pageNumber As Integer = 1
-                Dim description = New PdfSignatureFieldInfo(pageNumber)
-                description.Name = "SignatureField"
-                description.SignatureBounds = New PdfRectangle(10, 10, 50, 150)
-                ' Specify your Azure Key Vault URL - (vaultUri)
-                Const keyVaultUrl As String = ""
-                ' Specify the Azure Key Vault Certificate ID (certId)
-                Dim certificateId As String = ""
-                ' Specify the Azure Key Vault Key ID for a certificate
-                Dim keyId As String = ""
-                ' Create a custom signer object:
-                Dim client = AzureKeyVaultClient.CreateClient(keyVaultUrl)
-                Dim azureSigner As AzureKeyVaultSigner = New AzureKeyVaultSigner(client, certificateId, keyId, keyVaultUrl, tsaClient)
-                ' Apply a signature to a new form field:
-                Dim signatureBuilder = New PdfSignatureBuilder(azureSigner, description)
-                ' Specify an image and signer information:
-                signatureBuilder.SetImageData(IO.File.ReadAllBytes("signature.jpg"))
-                signatureBuilder.Location = "LOCATION"
-                ' Sign and save the document:
-                Dim output As String = "SignedDocument.pdf"
-                signer.SaveDocument(output, signatureBuilder)
-                Call Process.Start(New ProcessStartInfo(output) With {.UseShellExecute = True})
-            End Using
-#End Region
-        End Sub
-    End Class
+				' Specify your Azure Key Vault URL - (vaultUri)
+				Const keyVaultUrl As String = ""
+
+				' Specify the Name of and Azure Key Vault Certificate (certId).
+				' This is listed in the "Name" column of your Azure Portal, Certificates page
+				Dim certificateName As String = ""
+
+				' Specify the Version for the Azure Key Vault certificate.
+				' This is listed in the "Version" column of your Azure Portal, Certificates/[Certificate Name] page.
+				' Leave this empty, or null, to auto-select the "Current" version of the given certificate.
+				' Warning: Auto-selection will not "fall back" to a non-current certificate should the current certificate
+				'          be disabled, meaning that if the chosen certificate is disabled, signing will generate an error.  
+				Dim certificateVersion As String = ""
+
+				' Create a custom signer object:
+				Dim client = AzureKeyVaultClient.CreateClient(New Uri(keyVaultUrl))
+				Dim azureSigner As New AzureKeyVaultSigner(client, certificateName:= certificateName, certificateVersion:= certificateVersion, tsaClient)
+
+				' Apply a signature to a new form field:
+				Dim signatureBuilder = New PdfSignatureBuilder(azureSigner, description)
+
+				' Specify an image and signer information:
+				signatureBuilder.SetImageData(System.IO.File.ReadAllBytes("signature.jpg"))
+				signatureBuilder.Location = "LOCATION"
+
+				' Sign and save the document:
+				Dim output As String = "SignedDocument.pdf"
+				signer.SaveDocument(output, signatureBuilder)
+				Process.Start(New ProcessStartInfo(output) With {.UseShellExecute = True})
+			End Using
+'			#End Region
+		End Sub
+	End Class
 End Namespace
